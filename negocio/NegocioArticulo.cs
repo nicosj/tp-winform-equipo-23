81c062919c36f73fcca4009d97bde03509f0db76
Nicolas Rodriguez <nico.river09.9@gmail.com>
2023-05-03 00:17:57 -0300

    add: ConexionDB

--- negocio/NegocioArticulo.cs
+++ negocio/NegocioArticulo.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using ConexionDB;
 using Dominio;
 
 namespace negocio
@@ -10,11 +11,10 @@ namespace negocio
         public List<Articulo> listar()
         {
             List<Articulo> articulos = new List<Articulo>();
-            SqlConnection conn = new SqlConnection();
-            SqlCommand command = new SqlCommand();
-            SqlDataReader leer;
+            DB db = new DB();
+            
             try
-            {   //CADENA DE CONEXION NICO//
+            { /*  //CADENA DE CONEXION NICO//
                 conn.ConnectionString = "server=127.0.0.1;database=CATALOGO_P3_DB;uid=sa;pwd=Nicosj999@;encrypt=false";
                 //CADENA DE CONEXION SEBA//
                 //conn.ConnectionString = "server=localhost\\LABO3; database=CATALOGO_P3_DB; integrated security=false; user=sa; password=123xx;";
@@ -23,19 +23,28 @@ namespace negocio
                 command.CommandType = System.Data.CommandType.Text;
                 command.CommandText = "SELECT Id, Codigo, Nombre, Descripcion, IdMarca, IdCategoria, Precio FROM ARTICULOS";
                 command.Connection = conn;
-                conn.Open();
-                leer = command.ExecuteReader();
+                conn.Open();*/
+                
+                //leer = command.ExecuteReader();
 
-                while (leer.Read())
+                
+                
+                db.setearConsulta("SELECT Id, Codigo, Nombre, Descripcion, IdMarca, IdCategoria, Precio FROM ARTICULOS");
+                db.ejecutarLectura();
+                
+                
+                
+                
+                while (db.Lector.Read())
                 {
                     Articulo aux = new Articulo();
-                    aux.Id = leer.GetInt32(0);
-                    aux.Codigo = leer.GetString(1);
-                    aux.Nombre = leer.GetString(2);
-                    aux.Descripcion = leer.GetString(3);
-                    aux.IdMarca = (int)leer["IdMarca"];
-                    aux.IdCategoria =(int) leer["IdCategoria"];
-                    aux.Precio = (decimal)leer["Precio"];
+                    aux.Id = db.Lector.GetInt32(0);
+                    aux.Codigo = db.Lector.GetString(1);
+                    aux.Nombre = db.Lector.GetString(2);
+                    aux.Descripcion = db.Lector.GetString(3);
+                    aux.IdMarca = (int)db.Lector["IdMarca"];
+                    aux.IdCategoria =(int) db.Lector["IdCategoria"];
+                    aux.Precio = (decimal)db.Lector["Precio"];
                     articulos.Add(aux); 
                 }
                 return articulos;
