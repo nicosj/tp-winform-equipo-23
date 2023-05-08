6c68b09c38f93d1d284863dbc3a1b53458e22435
Nicolas Rodriguez <nico.river09.9@gmail.com>
2023-05-08 00:30:28 -0300

    add: icons

--- Catalogo/frmPrincipal.Designer.cs
+++ Catalogo/frmPrincipal.Designer.cs
@@ -138,21 +138,23 @@
 	        // 
 	        // button1
 	        // 
+	        this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
 	        this.button1.Location = new System.Drawing.Point(447, 147);
+	        this.button1.Margin = new System.Windows.Forms.Padding(0);
 	        this.button1.Name = "button1";
-	        this.button1.Size = new System.Drawing.Size(35, 25);
+	        this.button1.Size = new System.Drawing.Size(32, 32);
 	        this.button1.TabIndex = 11;
-	        this.button1.Text = "<-";
 	        this.button1.UseVisualStyleBackColor = true;
 	        this.button1.Click += new System.EventHandler(this.Previous_Click);
 	        // 
 	        // button2
 	        // 
+	        this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
 	        this.button2.Location = new System.Drawing.Point(641, 147);
+	        this.button2.Margin = new System.Windows.Forms.Padding(0);
 	        this.button2.Name = "button2";
-	        this.button2.Size = new System.Drawing.Size(35, 25);
+	        this.button2.Size = new System.Drawing.Size(32, 32);
 	        this.button2.TabIndex = 12;
-	        this.button2.Text = "->";
 	        this.button2.UseVisualStyleBackColor = true;
 	        this.button2.Click += new System.EventHandler(this.Next_Click);
 	        // 
